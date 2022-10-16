b6765b656b4941587379d43626d45330def3a355
Imakashkendre <akash.shopisthan@gmail.com>
2022-10-16 16:14:03 +0530

    Footer created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HomePage from "./Container/HomePage/HomePage";
 import NavBar from './Components/NavBar/NavBar';
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <>
       {/* <HomePage /> */}
       <NavBar />
+      <Footer />
     </>
   );
 }
