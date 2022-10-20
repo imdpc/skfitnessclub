92c5078a1c80c6a153d40a3026269ddead204289
Prasad Adhav <prasadadhav22@gmail.com>
2022-10-20 20:18:17 +0530

    change the footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import "./App.css";
 import HomePage from "./Container/HomePage/HomePage";
-import NavBar from './Components/NavBar/NavBar';
-import Footer from "./Components/Footer/Footer";
-
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      <NavBar />
-      <Footer />
+      <HomePage />
     </>
   );
 }
--- src/Container/HomePage/HomePage.js
+++ src/Container/HomePage/HomePage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./HomePage.css";
+// import Link from "react-router";
+import NavBar from "./../../Components/NavBar/NavBar";
+// import Footer from "../../Components/Footer/Footer";
+import SkFooter from "./../../Components/SkFooter/SkFooter";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <NavBar />
+      <div className="flex-container-for-the-tech-o-neers-screen-header">
+        {/* width  set for the screen */}
+        <div className="bgc-for-the-main-header-screen-image">
+          <div className="width-set-for-the-tech-o-neers-screen-header">
+            <div className="container-for-the-content-on-the-image">
+              <h1 className="header-for-the-img-section">
+                Create a Cohesive Network to Drive a Seamless Business Function
+              </h1>
+              <p className="info-for-the-img-section-after-header">
+                We are one stop shop solution for all your office essentials
+              </p>
+              {/* <Link to="/contact-us/"> */}
+              <button className="btn-for-discover-more-header-section">
+                Contact Us
+              </button>
+              {/* </Link> */}
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+      {/* <Footer /> */}
+      <SkFooter />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
